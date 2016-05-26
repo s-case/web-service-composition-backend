4b96b76267767bf1d06739d03daa81fdfadbd068
mkoutli <mkoutli@iti.gr>
2016-05-26 18:07:16 +0300

    first commit

---
+++ src/main/java/eu/scasefp7/sc/UploadFileService.java
@@ -0,0 +1,77 @@
+package eu.scasefp7.sc;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.sun.jersey.core.header.FormDataContentDisposition;
+import com.sun.jersey.multipart.FormDataParam;
+
+@Path("/file")
+public class UploadFileService {
+	static String tomcatWebAppsPath = "/home/ubuntu/apache-tomcat-8.0.30/webapps/";
+
+	@GET
+	@Path("/{fileName}")
+	@Produces("text/plain")
+	public String fileExists(@PathParam("fileName") String name) {
+
+		File file = new File(tomcatWebAppsPath + name);
+		System.out.println(file.getAbsolutePath());
+		if (file.exists())
+			return "true";
+		else
+			return "false";
+	}
+
+	@POST
+	@Path("/upload")
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response uploadFile(@FormDataParam("file") InputStream uploadedInputStream,
+			@FormDataParam("file") FormDataContentDisposition fileDetail) {
+
+		String uploadedFileLocation = tomcatWebAppsPath + fileDetail.getFileName();
+
+		// save it
+		writeToFile(uploadedInputStream, uploadedFileLocation);
+
+		String output = "File uploaded to : " + uploadedFileLocation;
+
+		return Response.status(200).entity(output).build();
+
+	}
+
+	// save uploaded file to new location
+	private void writeToFile(InputStream uploadedInputStream, String uploadedFileLocation) {
+
+		try {
+			OutputStream out = new FileOutputStream(new File(uploadedFileLocation));
+			int read = 0;
+			byte[] bytes = new byte[1024];
+
+			out = new FileOutputStream(new File(uploadedFileLocation));
+			while ((read = uploadedInputStream.read(bytes)) != -1) {
+				out.write(bytes, 0, read);
+			}
+			out.flush();
+			out.close();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
